4661c637bda875b8d07f9809d48d0e859f58bb84
jose_zena <jose.zena@globant.com>
2018-02-15 19:42:01 -0500

    encabezado casi terminado

---
+++ js/sitio.js
@@ -0,0 +1,14 @@
+// -------------------------
+// CABECERA ANIMADA
+// -------------------------
+$(window).scroll(function () {
+
+    var nav = $('.encabezado');
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 80) {
+        nav.addClass("fondo-menu");
+    } else {
+        nav.removeClass("fondo-menu");
+    }
+});
